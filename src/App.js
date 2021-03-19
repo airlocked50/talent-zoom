73a0fc97197dfe20d0e4c031abd71fd05efc6fe5
Vinicius Santos <vinicius.santos@ibm.com>
2021-03-19 18:35:06 -0300

    Initial github page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,17 @@ import './App.scss';
 import './App.css';
 
 import { LeadSpace, FeatureCardBlockLarge } from '@carbon/ibmdotcom-react';
-import { ArrowRight20 } from '@carbon/icons-react';
+import { Button } from 'carbon-components-react';
+import { ArrowRight20, Fade16, Menu32 } from '@carbon/icons-react';
+
+import HeaderContainer from "carbon-components-react/lib/components/UIShell/HeaderContainer";
+import {
+  Header,
+  HeaderGlobalBar,
+  SideNav,
+  SideNavItems,
+  SideNavMenuItem
+} from "carbon-components-react/lib/components/UIShell";
 
 class App extends Component {
 
@@ -17,12 +27,61 @@ class App extends Component {
         second_button.classList.remove("bx--btn--primary")
         second_button.classList.add("bx--btn--tertiary")
 
+    // document.querySelector('.bx--leadspace--content__container').classList.add("bx--grid")
+
+        
+
     console.log(document.querySelector('.bx--btn.bx--btn--tertiary'));
   }
 
   render() {
     return (
       <>
+
+        <HeaderContainer render={({ isSideNavExpanded, onClickSideNavExpand }) => (
+            <>
+              <Header aria-label="IBM Platform Name">
+
+                <a href="/" class="th--nav-logo">
+                  <img src="/images/logo.svg" alt="Talent Zoom" />
+                </a>
+
+                <HeaderGlobalBar>
+
+                  <div class="th--nav-links">
+                    <a href="/">Job Seeker</a>
+                    <a href="/">Employers</a>
+                    <a href="/">About Us</a>
+                    <a href="/">Login</a>
+                    <Button as="button" renderIcon={ArrowRight20} role="button">Sign Up</Button>
+                  </div>
+
+                  <Button 
+                    hasIconOnly 
+                    kind="ghost" 
+                    className="th--nav-menu"
+                    onClick={onClickSideNavExpand} 
+                    isActive={isSideNavExpanded} 
+                    renderIcon={Menu32} 
+                  />
+
+                </HeaderGlobalBar>
+
+                <SideNav aria-label="Side navigation" expanded={isSideNavExpanded}>
+                  <SideNavItems>
+                    <SideNavMenuItem href="javascript:void(0)">Job Seeker</SideNavMenuItem>
+                    <SideNavMenuItem href="javascript:void(0)">Employers</SideNavMenuItem>
+                    <SideNavMenuItem href="javascript:void(0)">About Us</SideNavMenuItem>
+                    <SideNavMenuItem href="javascript:void(0)">Login</SideNavMenuItem>
+                    <SideNavMenuItem href="javascript:void(0)">Sign Up</SideNavMenuItem>
+                  </SideNavItems>
+                </SideNav>
+
+              </Header>
+            </>
+          )}
+        />
+
         <LeadSpace
           type="default"
           theme="g100"
@@ -53,7 +112,7 @@ class App extends Component {
           }}
         />
 
-        <div class="bx--grid">
+        {/* <div class="bx--grid">
           <FeatureCardBlockLarge
             eyebrow="hello"
             heading="Want to work remote?"
@@ -64,7 +123,7 @@ class App extends Component {
               alt: 'lead space image',
             }}
           />
-        </div>
+        </div> */}
         
       </>
     );
